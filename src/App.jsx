17cc1dc4953df0ea139232585a8856a1b0a3eb8d
DC Isenberg <dclem742@gmail.com>
2024-10-18 19:42:26 -0400

    added animations to pre-search h1 and form, and gradient background

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useSpring, animated } from 'react-spring';
+import { useSpring, animated, config } from 'react-spring';
 import MovieList from './Components/MovieList';
 import SearchForm from './Components/SearchForm';
 import MovieDetails from './Components/MovieDetails';
@@ -8,12 +8,19 @@ function App() {
   const [searchTerm, setSearchTerm] = useState('');
   const [selectedMovie, setSelectedMovie] = useState(null);
   const [audio] = useState(new Audio('Avengers_Assemble.mp3'));
+
   const titleAnimation = useSpring({
     from: { transform: 'scale(0.1)', opacity: 0 },
     to: { transform: 'scale(1)', opacity: 1 },
     config: { duration: 1000 }
   });
-  
+
+  const formAnimation = useSpring({
+    from: { transform: 'scale(1.5)', opacity: 0 },
+    to: { transform: 'scale(1)', opacity: 1 },
+    config: { ...config.wobbly, duration: 800 },
+    delay: 2000 // This will make the form animation start after the title animation
+  });
 
   useEffect(() => {
     audio.loop = true;
@@ -36,10 +43,12 @@ function App() {
 
   return (
     <div className="App">
-          <animated.h1 style={titleAnimation} className="centered-title">
-            Marvelous Movies
-          </animated.h1>
-      <SearchForm onSearch={handleSearch} />
+      <animated.h1 style={titleAnimation} className="centered-title">
+        Marvelous Movies
+      </animated.h1>
+      <animated.div style={formAnimation}>
+        <SearchForm onSearch={handleSearch} />
+      </animated.div>
       {selectedMovie ? (
         <MovieDetails movie={selectedMovie} />
       ) : (
